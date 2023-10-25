d1a7e7fa6397537263a879570c0e45903c249f02
Harish Rao Takkallapally <takkallapally.h@northeastern.edu>
2023-10-25 19:42:35 -0400

    updated

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install -y nodejs npm
+
+sudo apt update
+
+sudo apt install -y unzip
+
+npm install express sequelize mysql2 body-parser bcrypt basic-auth
+
+npm uninstall bcrypt
+
+npm install bcrypt
+
+npm install csv-parser --save
+
+npm install chai-http --save-dev
+
+npm install
